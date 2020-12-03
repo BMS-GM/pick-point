286e11f67166576b0530cc9b44c2fadb95caf699
William Collicott <31823020+WilliamCollicott@users.noreply.github.com>
2020-12-03 12:47:09 -0500

    Small changes to boiler plate comments.

--- DepthMapDriver/standalone/StandaloneDepthMap.py
+++ DepthMapDriver/standalone/StandaloneDepthMap.py
@@ -1,12 +1,12 @@
 """
 --------------------------------------------------------------------
-Michigan  Technological University: Blue Marble Security Enterprise
+Michigan Technological University: Blue Marble Security Enterprise
 --------------------------------------------------------------------
 
 StandaloneDepthMap.py - standalone file containing functions to perform depthmap-related tasks
 
 Author: Will Collicott
-Date Last Modified 11/25/2020
+Date Last Modified 12/3/2020
 """
 
 import cv2
