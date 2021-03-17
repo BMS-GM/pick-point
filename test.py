776fc5c388c0deb8a83339d2931a117014516013
Hampton Pettinger <htpettin@mtu.edu>
2021-03-16 21:14:46 -0400

    Addition of speed and auto calibration control

---
+++ test.py
@@ -0,0 +1,16 @@
+'''
+Author: Hampton Pettinger
+2.3.21
+'''
+
+import tkinter as tk
+import time
+
+class Test():
+    def __init__(self):
+        self.start()
+
+
+if __name__ = '__main__':
+    bamboozle = Test()
+
